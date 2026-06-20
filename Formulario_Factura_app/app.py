dc1cec17bd25e4551d98dee2b3d16c4528fb0218
JMCisnerosP <sistemrespaldos@gmail.com>
2026-06-19 19:50:03 -0600

    Update email configuration to use environment variables

--- Formulario_Factura_app/app.py
+++ Formulario_Factura_app/app.py
@@ -15,7 +15,7 @@ app.config['MAIL_SERVER'] = 'smtp.zoho.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = '928381792'  # literal, no tu correo
+app.config['MAIL_USERNAME'] = os.environ.get('SENDGRID_SENDER') # literal, no tu correo
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('ZOHO_USER')
 
